e7ce787a5a72ff50eeabce757274f1886dc4b526
Boissard Clément <clement.boissard@etu.udamail.fr>
2016-05-20 10:40:12 +0200

    Échec du précédent commit

---
+++ Projet/ProjetData/XMLDataManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using ProjetLibrary;
+
+namespace ProjetData
+{
+    public class XMLDataManager : IDataManager
+    {
+        public string url { private set; get; }
+        DirectoryInfo dirInfo ;
+        string dirData;
+        XDocument userFile;
+
+        public XMLDataManager()
+        {
+            dirInfo = Directory.GetParent(Directory.GetCurrentDirectory());
+            dirData = dirInfo.FullName + "\\ProjetData\\XML";
+            userFile = new XDocument();
+        }
+
+        public XMLDataManager(string url) : this()
+        {
+            dirData = dirInfo.FullName + "\\ProjetData\\" + url;
+        }
+
+        public List<ICocktail> loadCocktail()
+        {
+        }
+
+        public List<IUser> loadUser()
+        {
+        }
+
+        public void saveCocktail(List<Cocktail> list)
+        {
+        }
+
+        public void saveUser(List<User> list)
+        {
+        }
+    }
+}
---
+++ Projet/ProjetLibrary/IIngredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetLibrary
+{
+    public interface IIngredient
+    {
+        string Nom { get; }
+        int Quantite { get; }
+        Unite Unite { get; }
+    }
+}
